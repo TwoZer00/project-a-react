0d307a59277316e09c16cdef648484bf6aa73841
Leobardo Verdugo <leobardo.21@outlook.es>
2022-05-30 12:56:56 -0600

    fixing

--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ import "./index.css";
 import { App } from "./App";
 import firebase from "./utils/firebase";
 import { getAnalytics } from "firebase/analytics";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 getAnalytics(firebase);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <HashRouter>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
