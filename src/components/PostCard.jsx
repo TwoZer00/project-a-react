67dc4e715224b6741ff3dcf95f541e1f2ac94e32
leo verdugo <leobardo@twozer00.dev>
2023-10-26 20:57:57 -0700

    minor fixes

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -66,7 +66,7 @@ export default function PostCard({ postData }) {
                     <CardContent sx={{ paddingY: 0, display: "flex", flexDirection: "column", gap: 1 }}>
                         {/* {postData.nsfw && <Typography sx={{ width: "fit-content", letterSpacing: 2, fontWeight: "bolder", fontSize: "12px" }} color='error'>NSFW</Typography>} */}
                         <Link component={RouterLink} to={`/categories/${postData.category.id}`} relative='path' underline='hover' color='text.primary' fontSize='small' sx={{ textDecoration: "none" }}>{((postData.category).path).substring(postData.category.path.lastIndexOf("/") + 1)}</Link>
-                        <Stack direction={"row"} gap={1} width={"100%"} overflow={'auto'} >
+                        <Stack direction={"row"} gap={1} width={"100%"} maxWidth={"350px"} overflow={'auto'} sx={{ scrollBehavior: "smooth", scrollbarWidth: "thin" }} >
                             {postData?.tags?.map((tag, index) => <Chip key={index} label={decodeURIComponent((tag.path).substring(tag.path.lastIndexOf("/") + 1))} clickable size='small' variant='outlined' component={RouterLink} to={`/tags/${(tag.path).substring(tag.path.lastIndexOf("/") + 1)}`} relative='path' />)}
                         </Stack>
                         <Stack direction={"column"} gap={1} maxWidth={"100%"}>
