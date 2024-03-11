e5af320c2e3ff43adbcb16f00b4d2c4c0d654935
twozer00 <leobardo.21@outlook.es>
2024-03-11 13:23:38 -0700

    redo

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,5 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { connectAuthEmulator, getAuth } from "firebase/auth";
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
+import { connectStorageEmulator, getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,12 +21,12 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-// if (location.hostname === "localhost") {
-//     const auth = getAuth();
-//     const db = getFirestore();
-//     const storage = getStorage();
-//     connectFirestoreEmulator(db, '127.0.0.1', 8080);
-//     connectAuthEmulator(auth, "http://127.0.0.1:9099", { disableWarnings: true });
-//     // Point to the Storage emulator running on localhost.
-//     connectStorageEmulator(storage, "127.0.0.1", 9199);
-// } 
+if (location.hostname === "localhost") {
+    const auth = getAuth();
+    const db = getFirestore();
+    const storage = getStorage();
+    connectFirestoreEmulator(db, '127.0.0.1', 8080);
+    connectAuthEmulator(auth, "http://127.0.0.1:9099", { disableWarnings: true });
+    // Point to the Storage emulator running on localhost.
+    connectStorageEmulator(storage, "127.0.0.1", 9199);
+} 
